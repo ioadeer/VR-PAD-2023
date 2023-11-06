a78b4a7984a2ba021cd6c8de8bbcebff66d06ed2
ioadeer <cjoackin@gmail.com>
2023-11-06 16:55:29 -0300

    Agregue analisis de intercept k y r squared al anterior. T test entre condiciones e histrogrmaa por condicion

--- analisis-pad-2-salas-vacias/analisis-4-coeficientes-a-k-r2.R
+++ analisis-pad-2-salas-vacias/analisis-4-coeficientes-a-k-r2.R
@@ -161,9 +161,7 @@ p3 + stat_compare_means(method = "t.test")
 
 t.test(r.squared~condicion_sala, data = data.clean)
 
-
-
-# histogramas -------------------------------------------------------------
+# histogramas de a exponente -------------------------------------------------------------
 
 # histogram de a (exponente)
 
@@ -325,7 +323,7 @@ bxp
 all <- ggarrange(
   bxp,
   ggarrange(
-    hist_a_sc,
+    hist_a_sc, 
     hist_a_sg,
     nrow = 2
   ),
@@ -337,3 +335,362 @@ plot(all)
 figures_folder = "./analisis-pad-2-salas-vacias/imgs_analisis_4_coefs"
 mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "coef_a.png", sep = '')
 ggsave(mi_nombre_de_archivo, plot = all, limitsize=FALSE, dpi=200)
+
+
+
+
+# histogramas de k intercept -------------------------------------------------------------
+
+# IQR(data.clean$intercept)
+# 
+# quantile(data.clean$intercept) # 75 % - 25 % = IQR
+# 
+# quantile(data.clean$intercept)[2]
+# quantile(data.clean$intercept)[4]
+
+#   "italic(k) == %.3g * ',' ~~ italic(a) == %.3g  * ',' ~~ italic(r)^2 ~ '=' ~ %.2g",
+
+
+# sala chica
+
+data.clean.sala_chica <- data.clean %>%
+  filter(condicion_sala == "SALA_CHICA")
+
+eqn1 <- sprintf(
+  "M = %.3g ± %.2g \nMdn = %.3g (%.2g - %.2g)\nN =  %.2g",
+  mean(data.clean.sala_chica$intercept),
+  sd(data.clean.sala_chica$intercept),
+  median(data.clean.sala_chica$intercept),
+  quantile(data.clean.sala_chica$intercept)[4],
+  quantile(data.clean.sala_chica$intercept)[2],
+  sum(data.clean.sala_chica$intercept)
+)
+
+hist_a_sc <- ggplot(data.clean.sala_chica, aes(intercept)) +
+  geom_histogram(binwidth= 0.5, color="black",fill="white") +
+  scale_x_continuous(breaks= seq(-3,1.0, by=1.0)) +
+  scale_y_continuous(breaks= seq(0,6, by = 1))+
+  xlim(-3,1) +
+  ylim(0,6) +
+  geom_vline(xintercept = round(mean(data.clean.sala_chica$intercept),2),        # Add line for mean
+             linetype="dashed",
+  ) +
+  annotate("text",                        # Add text for mean
+           #x = 1.5, # para fig sola
+           x = 0.0, # para fig compuesta
+           y = 5,
+           label = eqn1,
+           size = 1.5,
+           hjust = 0) +
+  labs(x="k", y = "Count")+
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank()) +
+  theme_minimal()
+
+hist_a_sc
+
+# sala grande
+
+data.clean.sala_grande <- data.clean %>%
+  filter(condicion_sala == "SALA_GRANDE")
+
+eqn1 <- sprintf(
+  "M = %.3g ± %.2g \nMdn = %.3g (%.2g - %.2g)\nN =  %.2g",
+  mean(data.clean.sala_grande$intercept),
+  sd(data.clean.sala_grande$intercept),
+  median(data.clean.sala_grande$intercept),
+  quantile(data.clean.sala_grande$intercept)[4],
+  quantile(data.clean.sala_grande$intercept)[2],
+  sum(data.clean.sala_grande$intercept)
+)
+
+
+# scale_x_continuous(breaks= seq(0.3,2.1, by=0.5)) +
+#   scale_y_continuous(breaks= seq(0,5, by = 1))+
+#   xlim(0.3,2) +
+#   ylim(0,5) +
+
+hist_a_sg <- ggplot(data.clean.sala_grande, aes(intercept)) +
+  geom_histogram(binwidth= 0.5, color="black",fill="white") +
+  geom_vline(xintercept = round(mean(data.clean.sala_grande$intercept),2),        # Add line for mean
+             linetype="dashed",
+  ) +
+  scale_x_continuous(breaks= seq(-3,1.0, by=1.0)) +
+  scale_y_continuous(breaks= seq(0,6, by = 1))+
+  xlim(-3,1) +
+  ylim(0,6) +
+  annotate("text",                        # Add text for mean
+           #x = 1.5, # para fig sola
+           x = 0.0, # para fig compuesta
+           y = 5.0,
+           label = eqn1,
+           size = 1.5,
+           hjust = 0) +
+  labs(x="a", y = "Count")+
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank()) +
+  theme_minimal()
+
+hist_a_sg
+
+a_comp <- ggarrange(
+  hist_a_sc,
+  hist_a_sg,
+  nrow = 2
+)
+
+plot(a_comp)
+
+
+figures_folder = "./analisis-pad-2-salas-vacias/imgs_analisis_4_intercepts"
+mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "2_histogramas_a.png", sep = '')
+ggsave(mi_nombre_de_archivo, plot = a_comp, limitsize=FALSE, dpi=200)
+
+
+# histograma a apaisado
+data.clean.boxplot_a <- data.clean %>%
+  mutate(
+    condicion_sala = case_when(
+      condicion_sala == "SALA_GRANDE" ~ "Grande",
+      condicion_sala == "SALA_CHICA" ~ "Chica",
+    )
+  )
+
+bxp <- ggboxplot(data.clean.boxplot_a, x = "condicion_sala", y = "intercept",
+                 orientation = "horizontal",
+                 color = "condicion_sala", palette = "jco",
+                 add = "jitter", ylab="intercept a",
+                 title="T-test de k") +
+  theme_minimal() +
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.title = element_text(hjust = 0),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank())
+
+
+stat.test <- data.clean.boxplot_a  %>% 
+  t_test(intercept~condicion_sala) %>%
+  add_significance()
+
+
+stat.test <- stat.test %>% add_y_position(fun ="mean", step.increase = 10)
+
+
+bxp <- bxp + stat_pvalue_manual(
+  stat.test, label = "p", tip.length = 0.01,
+  coord.flip = TRUE
+) +
+  coord_flip()
+
+bxp
+
+all <- ggarrange(
+  bxp,
+  ggarrange(
+    hist_a_sc, 
+    hist_a_sg,
+    nrow = 2
+  ),
+  ncol = 2
+)
+
+plot(all)
+
+figures_folder = "./analisis-pad-2-salas-vacias/imgs_analisis_4_intercepts"
+mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "intercept_k.png", sep = '')
+ggsave(mi_nombre_de_archivo, plot = all, limitsize=FALSE, dpi=200)
+
+
+
+
+
+
+# histogramas de r squared -------------------------------------------------------------
+
+# IQR(data.clean$r.squared)
+# 
+# quantile(data.clean$r.squared) # 75 % - 25 % = IQR
+# 
+# quantile(data.clean$r.squared)[2]
+# quantile(data.clean$r.squared)[4]
+
+#   "italic(k) == %.3g * ',' ~~ italic(a) == %.3g  * ',' ~~ italic(r)^2 ~ '=' ~ %.2g",
+
+
+# sala chica
+
+data.clean <- data.clean  %>%
+  rename(rsqr = r.squared)
+
+data.clean.sala_chica <- data.clean %>%
+  filter(condicion_sala == "SALA_CHICA")
+
+eqn1 <- sprintf(
+  "M = %.3g ± %.2g \nMdn = %.3g (%.2g - %.2g)\nN =  %.2g",
+  mean(data.clean.sala_chica$rsqr),
+  sd(data.clean.sala_chica$rsqr),
+  median(data.clean.sala_chica$rsqr),
+  quantile(data.clean.sala_chica$rsqr)[4],
+  quantile(data.clean.sala_chica$rsqr)[2],
+  sum(data.clean.sala_chica$rsqr)
+)
+
+hist_a_sc <- ggplot(data.clean.sala_chica, aes(rsqr)) +
+  geom_histogram(binwidth= 0.1, color="black",fill="white") +
+  scale_x_continuous(breaks= seq(0.3,1.0, by=0.1)) +
+  scale_y_continuous(breaks= seq(0,12, by = 1))+
+  xlim(0.5,1.1) +
+  ylim(0,13) +
+  geom_vline(xintercept = round(mean(data.clean.sala_chica$rsqr),2),        # Add line for mean
+             linetype="dashed", color = 'darkgrey'
+  ) +
+  annotate("text",                        # Add text for mean
+           #x = 1.5, # para fig sola
+           x = 0.50, # para fig compuesta
+           y = 10.0,
+           label = eqn1,
+           size = 1.5,
+           hjust = 0) +
+  labs(x=expression("R"^2*""), y = "Count")+
+  # (m"^3*")
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank()) +
+  theme_minimal()
+
+hist_a_sc
+
+# sala grande
+
+data.clean.sala_grande <- data.clean %>%
+  filter(condicion_sala == "SALA_GRANDE")
+
+eqn1 <- sprintf(
+  "M = %.3g ± %.2g \nMdn = %.3g (%.2g - %.2g)\nN =  %.2g",
+  mean(data.clean.sala_grande$rsqr),
+  sd(data.clean.sala_grande$rsqr),
+  median(data.clean.sala_grande$rsqr),
+  quantile(data.clean.sala_grande$rsqr)[4],
+  quantile(data.clean.sala_grande$rsqr)[2],
+  sum(data.clean.sala_grande$rsqr)
+)
+
+
+# scale_x_continuous(breaks= seq(0.3,2.1, by=0.5)) +
+#   scale_y_continuous(breaks= seq(0,5, by = 1))+
+
+hist_a_sg <- ggplot(data.clean.sala_grande, aes(rsqr)) +
+  geom_histogram(binwidth= 0.1, color="black",fill="white") +
+  geom_vline(xintercept = round(mean(data.clean.sala_grande$rsqr),2),        # Add line for mean
+             linetype="dashed", col = 'darkgrey',
+  ) +
+  scale_x_continuous(breaks= seq(0.3,1.0, by=0.1)) +
+  scale_y_continuous(breaks= seq(0,12, by = 1))+
+  xlim(0.5,1.1) +
+  ylim(0,13) +
+  annotate("text",                        # Add text for mean
+           #x = 1.5, # para fig sola
+           x = 0.50, # para fig compuesta
+           y = 10.0,
+           label = eqn1,
+           size = 1.5,
+           hjust = 0) +
+  labs(x=expression("R"^2*""), y = "Count")+
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank()) +
+  theme_minimal()
+
+hist_a_sg
+
+r_comp <- ggarrange(
+  hist_a_sc,
+  hist_a_sg,
+  nrow = 2
+)
+
+plot(r_comp)
+
+
+figures_folder = "./analisis-pad-2-salas-vacias/imgs_analisis_4_r.squareds"
+mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "2_histogramas_r2.png", sep = '')
+ggsave(mi_nombre_de_archivo, plot = r_comp, limitsize=FALSE, dpi=200)
+
+
+# histograma a apaisado
+data.clean.boxplot_a <- data.clean %>%
+  mutate(
+    condicion_sala = case_when(
+      condicion_sala == "SALA_GRANDE" ~ "Grande",
+      condicion_sala == "SALA_CHICA" ~ "Chica",
+    )
+  )
+
+bxp <- ggboxplot(data.clean.boxplot_a, x = "condicion_sala", y = "rsqr",
+                 orientation = "horizontal",
+                 color = "condicion_sala", palette = "jco",
+                 add = "jitter", ylab="r.squared a",
+                 title="T test R squared") +
+  theme_minimal() +
+  theme(
+    legend.position="none",
+    #axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    #axis.title = element_text(hjust = 0),
+    #axis.ticks = element_blank(),
+    axis.title.y = element_blank())
+
+
+stat.test <- data.clean.boxplot_a  %>% 
+  t_test(rsqr~condicion_sala) %>%
+  add_significance()
+
+
+stat.test <- stat.test %>% add_y_position(fun ="mean", step.increase = -7)
+
+
+bxp <- bxp + stat_pvalue_manual(
+  stat.test, label = "p", tip.length = 0.01,
+  coord.flip = TRUE
+) +
+  coord_flip()
+
+bxp
+
+all <- ggarrange(
+  bxp,
+  ggarrange(
+    hist_a_sc, 
+    hist_a_sg,
+    nrow = 2
+  ),
+  ncol = 2
+)
+
+plot(all)
+
+figures_folder = "./analisis-pad-2-salas-vacias/imgs_analisis_4_r_squared"
+mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "r_squared.png", sep = '')
+ggsave(mi_nombre_de_archivo, plot = all, limitsize=FALSE, dpi=200)
+
+
+
+
+
