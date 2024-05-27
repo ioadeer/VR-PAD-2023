9e4529d434a37de12baeaa6fb4dce98daaa1c9de
ioadeer <cjoackin@gmail.com>
2024-05-27 12:11:24 -0300

    Generando figuras para resultados del experimento 2

--- Exp_2_ADP_control/figure_generator.R
+++ Exp_2_ADP_control/figure_generator.R
@@ -3,7 +3,6 @@
 #  Un grafico con el modelo, y las medidas promedio de PAD
 #  Por otro lado tener las dos figuras de sesgo
 
-
 # dependencies ------------------------------------------------------------
 library(tidyverse)
 #library(Routliers)
@@ -27,17 +26,23 @@ library(effects)
 
 # load data and generate image  ---------------------------------------------------------------
 
+
+#theme_set(theme_gray(base_family = "DejaVuSerif"))
 rm(list=ls())
-figures_folder = "Figura_final"
+figures_folder = "Exp_2_ADP_control/Figura_final"
 results_tbl <- read.csv("Exp_2_ADP_control/ResultsData/Dresults.csv", header = TRUE, sep = ',', stringsAsFactors = TRUE)
 
 cbPalette <- c("#000000","#E69F00","#009E73", "#999999", "#D55E00", "#0072B2", "#CC79A7", "#F0E442")
 
+results_tbl <- results_tbl %>%
+  mutate(
+    perc_dist_log_10 = log10(perc_dist)
+  )
 
 m.Dist1 <-  lmer(log10(perc_dist) ~ log10(target_distance)*room_condition+(1+log10(target_distance)|subject)+(0+room_condition|subject),
                  data = results_tbl)
 
-Final.Fixed<-effect(c("log10(target_distance)*room_condition"), m.Dist1)
+#Final.Fixed<-effect(c("log10(target_distance)*room_condition"), m.Dist1)
 
 Final.Fixed<-effect(c("log10(target_distance)*room_condition"), m.Dist1, xlevels=list(target_distance=seq(2,9,1)))
 #2,2.7,3.65,4.9,6.65,9
@@ -63,12 +68,12 @@ eq3 <- substitute("r.squared:"~~~italic(R)^italic(2) == italic(b),
 
 
 tabla.pob = results_tbl %>% group_by(target_distance,room_condition) %>%
-  summarise(Mperc_dist  = mean(perc_dist),
-            SDperc_dist = sd(perc_dist)/sqrt(n()))  %>%
+  summarise(Mperc_dist  = mean(perc_dist_log_10),
+            SDperc_dist = sd(perc_dist_log_10)/sqrt(n()))  %>%
   ungroup()
 
-f1 <- ggplot(tabla.pob, aes(x=target_distance, y =Mperc_dist, group = room_condition, color  = room_condition)) +
-  geom_pointrange(aes(x = target_distance, y = Mperc_dist, ymin = Mperc_dist-SDperc_dist, ymax = Mperc_dist+SDperc_dist),alpha = 1,
+f1 <- ggplot(tabla.pob, aes(x=target_distance, y =10^Mperc_dist, group = room_condition, color  = room_condition)) +
+  geom_pointrange(aes(x = target_distance, y = 10^Mperc_dist, ymin = 10^(Mperc_dist-SDperc_dist), ymax = 10^(Mperc_dist+SDperc_dist)),alpha = 1,
                   position = position_jitterdodge(jitter.width = .1,
                                                   jitter.height = 0,
                                                   dodge.width = .1 ))+
@@ -76,16 +81,8 @@ f1 <- ggplot(tabla.pob, aes(x=target_distance, y =Mperc_dist, group = room_condi
   scale_colour_manual(values = cbPalette) +
   scale_fill_manual(values = cbPalette) +
   geom_line(data = Final.Fixed, aes(x = target_distance, y =10^fit, group=room_condition, color=room_condition))+
-  #geom_abline(slope =m.Dist1$coefficients$fixed[[2]],
-  #            intercept =m.Dist1$coefficients$fixed[[1]],
-  #            alpha = 0.5,
-  #            color = "#000000") +
-  #geom_abline(slope =m.Dist1$coefficients$fixed[[2]]+m.Dist1$coefficients$fixed[[4]],
-  #            intercept =m.Dist1$coefficients$fixed[[1]]+m.Dist1$coefficients$fixed[[3]],
-  #            alpha = 0.5,
-  #            color = "#E69F00") +
-  geom_text(x = 0.2, y = 7.0, label = as.character(as.expression(eq1)), hjust = 0, nudge_x =  0, parse = TRUE, size = 4, color = "#000000")+
-  geom_text(x = 0.2, y = 6.5, label = as.character(as.expression(eq2)), hjust = 0, nudge_x =  0,parse = TRUE, size = 4, color = "#E69F00")+
+  geom_text(x = 0.2, y = 8.0, label = as.character(as.expression(eq1)), hjust = 0, nudge_x =  0, parse = TRUE, size = 4, color = "#000000")+
+  geom_text(x = 0.2, y = 7.0, label = as.character(as.expression(eq2)), hjust = 0, nudge_x =  0,parse = TRUE, size = 4, color = "#E69F00")+
   geom_text(x = 0.2, y = 6.0, label = as.character(as.expression(eq3)), hjust = 0, nudge_x =  0, parse = TRUE, size = 4, color = "#009E73")+
   scale_x_continuous(name="Distance source (m)", limits = c(0,10)) +
   scale_y_continuous(name="Perceived distance (m)",   limits = c(0,10)) +
@@ -95,8 +92,124 @@ f1 <- ggplot(tabla.pob, aes(x=target_distance, y =Mperc_dist, group = room_condi
 
 
 f1
-mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "5. Lme Lineal-Normal", ".jpg", sep = '')
-ggsave(mi_nombre_de_archivo, plot=f1, width=15, height=15, units="cm", limitsize=FALSE, dpi=600)
+#mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "5. Lme Lineal-Normal", ".jpg", sep = '')
+#ggsave(mi_nombre_de_archivo, plot=f1, width=15, height=15, units="cm", limitsize=FALSE, dpi=600)
+
+
+# signed bias -------------------------------------------------------------
+
+results_tbls <- results_tbl %>% 
+  group_by(room_condition,subject) %>%
+  summarise(mBiasSigned  = mean(rel_bias_signed,na.rm=TRUE),
+            SdBiasSigned  = sd(rel_bias_signed,na.rm=TRUE)/sqrt(length(rel_bias_signed)),
+            mBiasUnSigned  = mean(rel_bias_unsigned,na.rm=TRUE),
+            SdBiasUnSigned  = sd(rel_bias_unsigned,na.rm=TRUE)/sqrt(length(rel_bias_unsigned)))  %>%
+  ungroup()
+
+results_tblp <- results_tbls %>% 
+  group_by(room_condition) %>%
+  summarise(MBiasSigned  = mean(mBiasSigned,na.rm=TRUE),
+            SDBiasSigned  = sd(mBiasSigned,na.rm=TRUE)/sqrt(length(mBiasSigned)),
+            MBiasUnSigned  = mean(mBiasUnSigned,na.rm=TRUE),
+            SDBiasUnSigned  = sd(mBiasUnSigned,na.rm=TRUE)/sqrt(length(mBiasUnSigned)))  %>%
+  ungroup()
+# geom_flat_violin(aes(fill = Condicion),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+#   geom_point(aes(x = as.numeric(Tiempo)-.15, y = LeqAS, colour = Condicion),position = position_jitter(width = .05, height = 0), size = 1, shape = 20)+
+
+f6 <-  ggplot(results_tblp, aes(x = room_condition,y = MBiasSigned, colour = room_condition, fill = room_condition)) +
+  geom_pointrange(aes(x=room_condition, y=MBiasSigned, ymin=MBiasSigned-SDBiasSigned, ymax=MBiasSigned+SDBiasSigned), size = 1.2)+
+  geom_line(aes(group = room_condition),size = 1.2, alpha=.5)+
+  geom_point(data = results_tbls, mapping = aes(x = room_condition,y = mBiasSigned, colour = room_condition, fill = room_condition), alpha = .8)+
+  geom_line(data = results_tbls, mapping = aes(x = room_condition,y = mBiasSigned, group = subject, colour = room_condition),alpha = 0.3)+
+  scale_colour_manual(values = cbPalette) + 
+  scale_fill_manual(values = cbPalette) + 
+  geom_abline(slope = 0,
+              intercept = 0,
+              alpha = 0.5,
+              linetype = "dashed") +
+  labs(x = "Condition", 
+       y = "Relative signed \nbias [%]") +
+  # facet_grid(. ~ type) +
+  annotate("text", x = 1.5, y = 0.3,  label = "*", size = 4) +
+  annotate("segment", x = 1, xend = 2, y = 0.2, yend = 0.2, colour = "black", size=.5, alpha=1,)+
+  theme_pubr(base_size = 12, margin = TRUE)+
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank())
+
+f6
+
+m.RelativBias <- lm(mBiasSigned ~ room_condition, 
+                    data = results_tbls)
+extract_stats(ggcoefstats(m.RelativBias))
+anov = anova(m.RelativBias)
+anov
+f6
+#mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "13. Bias signed", ".png", sep = '')
+#ggsave(mi_nombre_de_archivo, plot=f6, width=15, height=10, units="cm", limitsize=FALSE, dpi=600)
+
+testSigendBias <- t.test(filter(results_tbls, 
+              room_condition=="No visual information" )$mBiasSigned,
+       filter(results_tbls, 
+              room_condition=="Visual information")$mBiasSigned, 
+       paired = TRUE)
+
+# unsigned bias -----------------------------------------------------------
+
+f7 =  ggplot(results_tblp, aes(x = room_condition,y = MBiasUnSigned, colour = room_condition, fill = room_condition)) +
+  geom_pointrange(aes(x=room_condition, y=MBiasUnSigned, ymin=MBiasUnSigned-SDBiasUnSigned, ymax=MBiasUnSigned+SDBiasUnSigned), size = 1.2)+
+  geom_line(aes(group = room_condition),size = 1.2, alpha=.5)+
+  geom_point(data = results_tbls, mapping = aes(x = room_condition,y = mBiasUnSigned, colour = room_condition, fill = room_condition), alpha = .8)+
+  geom_line(data = results_tbls, mapping = aes(x = room_condition,y = mBiasUnSigned, group = subject, colour = room_condition),alpha = 0.3)+
+  scale_colour_manual(values = cbPalette) + 
+  scale_fill_manual(values = cbPalette) + 
+  geom_abline(slope = 0,
+              intercept = 0,
+              alpha = 0.5,
+              linetype = "dashed") +
+  labs(x = "Condition", 
+       y = "Relative unsigned \nbias [%]") +
+  # facet_grid(. ~ type) +
+  annotate("text", x = 1.5, y = 1.1,  label = "***", size = 4) +
+  annotate("segment", x = 1, xend = 2, y = 1.0, yend = 1.0, colour = "black", size=.5, alpha=1,)+
+  theme_pubr(base_size = 12, margin = TRUE)+
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank())
+
+f7
+
+m.RelativUnsignedBias <- lm(mBiasUnSigned ~ room_condition, 
+                            data = results_tbls)
+extract_stats(ggcoefstats(m.RelativUnsignedBias))
+anov = anova(m.RelativUnsignedBias)
+anov
+f7
+#mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "13. Bias signed", ".png", sep = '')
+#ggsave(mi_nombre_de_archivo, plot=f6, width=15, height=10, units="cm", limitsize=FALSE, dpi=600)
+
+t.test(filter(results_tbls, 
+              room_condition=="No visual information" )$mBiasUnSigned,
+       filter(results_tbls, 
+              room_condition=="Visual information")$mBiasUnSigned, 
+       paired = TRUE)
 
 
 
+# main plot ---------------------------------------------------------------
+
+#f1 f6 y f7
+main_figure <- ggarrange(f1, 
+                    ggarrange(f6, f7, widths = c(2,2),
+                              ncol = 2, labels = c("B", "C")),
+                    nrow = 2, 
+                    labels ="A",
+                    heights = c(1, 0.75),
+                    common.legend = TRUE)
+#                    legend = "top")
+
+figures_folder = "./Exp_2_ADP_control/Figura_final/"
+mi_nombre_de_archivo = paste(figures_folder, .Platform$file.sep, "exp", ".png", sep = '')
+ggsave(mi_nombre_de_archivo, plot=main_figure, width=15, height=15, units="cm", limitsize=FALSE, dpi=600)
+
+main_figure
